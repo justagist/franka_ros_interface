7e7d3840679b0fb885e8a0b51be28bc90f39db6f
JustaGist <saifksidhik@gmail.com>
2019-11-11 21:08:46 +0000

    add trajectory action client

--- franka_tools/src/franka_tools/__init__.py
+++ franka_tools/src/franka_tools/__init__.py
@@ -1,2 +1,3 @@
 from .frames_interface import FrankaFramesInterface
+from .joint_trajectory_action_client import JointTrajectoryActionClient
 from .controller_manager_interface import FrankaControllerManagerInterface
