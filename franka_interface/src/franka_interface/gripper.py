829ac894c76ceafa38aadf48f7d6721ab9e52082
JustaGist <saifksidhik@gmail.com>
2019-11-14 22:51:28 +0000

    complete gripper interface

--- franka_interface/src/franka_interface/gripper.py
+++ franka_interface/src/franka_interface/gripper.py
@@ -9,21 +9,36 @@
 #
 
 import rospy
+import actionlib
 import franka_dataflow
 from copy import deepcopy
 from sensor_msgs.msg import JointState
 
+from actionlib_msgs.msg import GoalStatus
+
+from franka_gripper.msg import ( GraspAction, GraspGoal, 
+                                 HomingAction, HomingGoal,   
+                                 MoveAction, MoveGoal,
+                                 StopAction, StopGoal,
+                                 GraspEpsilon )
+
 class GripperInterface(object):
     """
-    Interface class for a gripper on the Franka Panda Robot.
+    Interface class for the gripper on the Franka Panda robot.
     """
 
-    def __init__(self, gripper_joint_names, ns = 'franka', calibrate=True):
+    def __init__(self, gripper_joint_names = ['panda_finger_joint1', 'panda_finger_joint2'], ns = 'franka_ros_interface', calibrate = False):
         """
         Constructor.
 
-        @type calibrate: bool
-        @param calibrate: Attempts to calibrate the gripper when initializing class (defaults True)
+        @param gripper_joint_names    : Names of the finger joints
+        @param ns                     : base namespace of interface ('frank_ros_interface'/'panda_simulator')
+        @param calibrate              : Attempts to calibrate the gripper when initializing class (defaults True)
+
+        @type calibrate               : bool
+        @type gripper_joint_names     : [str]
+        @type ns                      : str
+
         """
         
         self.name = ns + '/franka_gripper'
@@ -42,6 +57,28 @@ class GripperInterface(object):
             self._exists = False
             return 
         self._exists = True
+
+        if not self._exists:
+            return
+
+
+        self._homing_action_client = actionlib.SimpleActionClient("{}homing".format(ns), HomingAction)
+
+        self._grasp_action_client = actionlib.SimpleActionClient("{}grasp".format(ns), GraspAction)
+
+        self._move_action_client = actionlib.SimpleActionClient("{}move".format(ns), MoveAction)
+
+        self._stop_action_client = actionlib.SimpleActionClient("{}stop".format(ns), StopAction)
+
+        self._homing_action_client.wait_for_server()
+        self._grasp_action_client.wait_for_server()
+        self._move_action_client.wait_for_server()
+        self._stop_action_client.wait_for_server()
+
+        if calibrate:
+            self.home_joints(wait_for_result = True)
+
+
 
     @property
     def exists(self):
@@ -70,8 +107,9 @@ class GripperInterface(object):
         """
         Return the requested joint position.
 
-        @type joint: str
-        @param joint: name of a joint
+        @param joint    : name of a joint
+        @type joint     : str
+
         @rtype: float
         @return: position individual joint
         """
@@ -99,8 +137,9 @@ class GripperInterface(object):
         """
         Return the requested joint velocity.
 
-        @type joint: str
-        @param joint: name of a joint
+        @param joint    : name of a joint
+        @type joint     : str
+
         @rtype: float
         @return: velocity in radians/s of individual joint
         """
@@ -119,8 +158,9 @@ class GripperInterface(object):
         """
         Return the requested joint effort.
 
-        @type joint: str
-        @param joint: name of a joint
+        @param joint    : name of a joint
+        @type joint     : str
+
         @rtype: float
         @return: effort in Nm of individual joint
         """
@@ -135,4 +175,126 @@ class GripperInterface(object):
         """
         return deepcopy(self._joint_effort)
 
+    def _active_cb(self):
+        rospy.loginfo("GripperInterface: '{}' request active.".format(self._caller))
+
+    def _feedback_cb(self, msg):
+        rospy.loginfo("GripperInterface: '{}' request feedback: \n\t{}".format(self._caller,msg))
+
+    def _done_cb(self, status, result):
+        rospy.loginfo("GripperInterface: '{}' complete. Result: \n\t{}".format(self._caller, result))
+
+
+    def home_joints(self, wait_for_result = False):
+        """
+        Performs homing of the gripper.
+       
+        After changing the gripper fingers, a homing needs to be done.
+        This is needed to estimate the maximum grasping width.
+
+        @param wait_for_result  : if True, this method will block till response is 
+                                    recieved from server
+        @type wait_for_result   : bool
+       
+        @return success
+        @rtype bool      
+        
+        """
+        self._caller = "home_joints"
+
+        goal = HomingGoal()
+
+        self._homing_action_client.send_goal(goal, done_cb =self._done_cb, active_cb = self._active_cb, feedback_cb = self._feedback_cb)
+
+        if wait_for_result:
+            result = self._homing_action_client.wait_for_result(rospy.Duration(15.))
+            return result
+
+        return True
+
+    def move_joints(self, width, speed = 0.05, wait_for_result = True):
+        """
+        Moves the gripper fingers to a specified width.
+       
+        @param width            : Intended opening width. [m]
+        @param speed            : Closing speed. [m/s]
+        @param wait_for_result  : if True, this method will block till response is 
+                                    recieved from server
+
+        @type width             : float
+        @type speed             : float
+        @type wait_for_result   : bool
+       
+        @return True if command was successful, False otherwise.
+        @rtype bool
+        """
+        self._caller = "move_joints"
+
+        goal = MoveGoal()
+        goal.width = width
+        goal.speed = speed
+
+        self._move_action_client.send_goal(goal, done_cb =self._done_cb, active_cb = self._active_cb, feedback_cb = self._feedback_cb)
+
+        if wait_for_result:
+            result = self._move_action_client.wait_for_result(rospy.Duration(15.))
+            return result
+
+        return True
+
+
+    def stop_action(self):
+        """
+        Stops a currently running gripper move or grasp.
+       
+        @return True if command was successful, False otherwise.
+        @rtype bool
+        """
+        self._caller = "stop_action"
+
+        goal = StopGoal()
+
+        self._stop_action_client.send_goal(goal, done_cb =self._done_cb, active_cb = self._active_cb, feedback_cb = self._feedback_cb)
+
+        result = self._stop_action_client.wait_for_result(rospy.Duration(15.))
+        return result
+
+    def grasp(self, width, force, speed = 0.04, epsilon_inner = 0.005, epsilon_outer = 0.005,wait_for_result = True):
+        """
+        Grasps an object.
+       
+        An object is considered grasped if the distance $d$ between the gripper fingers satisfies
+        $(\text{width} - \text{epsilon_inner}) < d < (\text{width} + \text{epsilon_outer})$.
+       
+        @param width        : Size of the object to grasp. [m]
+        @param speed        : Closing speed. [m/s]
+        @param force        : Grasping force. [N]
+        @param epsilon_inner: Maximum tolerated deviation when the actual grasped width is smaller
+                                than the commanded grasp width.
+        @param epsilon_outer: Maximum tolerated deviation when the actual grasped width is wider
+                                than the commanded grasp width.
+
+        @type width         : float
+        @type speed         : float
+        @type force         : float
+        @type epsilon_inner : float
+        @type epsilon_outer : float
+
+        @return True if an object has been grasped, false otherwise.
+        """
+        self._caller = "grasp_action"
+
+        goal = GraspGoal()
+        goal.width = width
+        goal.speed = speed
+        goal.force = force
+        goal.epsilon = GraspEpsilon(inner = epsilon_inner, outer = epsilon_outer)
+
+        self._grasp_action_client.send_goal(goal, done_cb =self._done_cb, active_cb = self._active_cb, feedback_cb = self._feedback_cb)
+
+        if wait_for_result:
+            result = self._grasp_action_client.wait_for_result(rospy.Duration(15.))
+            return result
+
+        return True
 
