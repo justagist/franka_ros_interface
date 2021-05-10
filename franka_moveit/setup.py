8cec0c87ec376be93d79da486194992b198e04f8
JustaGist <saifksidhik@gmail.com>
2021-05-10 16:02:08 +0100

    :adhesive_bandage: [IMPROVEMENT] Fix shebang compatibility hack (#31)
    - Fix shebang issue by updating CMakeLists for Python3 support with Python2 backward compatibility (thanks @matteolucchi)
    - Remove shebangs from unnecessary files; change executability settings for some files
    - Update/add settings.py for required packages

--- franka_moveit/setup.py
+++ franka_moveit/setup.py
@@ -1,11 +1,4 @@
-#!/bin/sh
-''':'
-if [ "$ROS_PYTHON_VERSION" = "3" ]; then
-  exec python3 "$0" "$@"
-else
-  exec python2 "$0" "$@"
-fi
-'''
+#!/usr/bin/env python
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
